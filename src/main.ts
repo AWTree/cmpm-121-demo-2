e0088c97ad55a79fa79ce075cc1e288353d6efe3
AWTree <arnowu320@gmail.com>
2024-10-16 17:12:57 -0700

    Step 1 Completed

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,15 @@
 import "./style.css";
 
-const APP_NAME = "Hello";
+const APP_NAME = "My Sticker Sketchpad";
 const app = document.querySelector<HTMLDivElement>("#app")!;
 
 document.title = APP_NAME;
-app.innerHTML = APP_NAME;
+
+const title = document.createElement("h1");
+title.textContent = APP_NAME;
+app.appendChild(title);
+
+const canvas = document.createElement("canvas");
+canvas.width = 256;
+canvas.height = 256;
+app.appendChild(canvas);
